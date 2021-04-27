144611288478f3148cfc39e80724be4fa7979911
Juraj Hrivnák <71150936+juraj-hrivnak@users.noreply.github.com>
2021-04-27 11:59:30 +0200

    Update setup.bat

--- automation/setup.bat
+++ automation/setup.bat
@@ -1,5 +1,6 @@
 @echo off
 
+cd ..
 type NUL > .git/hooks/post-merge
 echo #!/bin/sh > .git/hooks/post-merge
 echo java -jar InstanceSync.jar >> .git/hooks/post-merge
@@ -8,5 +9,3 @@ echo Done setting up hooks
 echo Running InstanceSync
 
 java -jar InstanceSync.jar
-
-pause
