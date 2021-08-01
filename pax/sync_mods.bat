f975aea412c73dd7cc75aaad319097cd8bc6c4fa
Juraj Hrivnák <71150936+juraj-hrivnak@users.noreply.github.com>
2021-08-01 13:49:21 +0200

    Added script for syncing and downloading mods
    Using `sync_mods.bat` and `ModpackDownloader`

---
+++ pax/sync_mods.bat
@@ -0,0 +1,13 @@
+
+cd ..
+if not exist "mods" mkdir "mods"
+cd mods
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+cd ..\pax\modpack\overrides
+xcopy /s mods ..\..\..\mods
+
+cd ..\..
+cmd /c java -jar ModpackDownloader-cli-0.6.1.jar -manifest modpack/manifest.json -folder ../mods
+
+echo Done!
+pause
