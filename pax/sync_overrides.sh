181560a57c4e1952c78f9ceff11306dd0d2f23d3
Juraj Hrivnák <71150936+juraj-hrivnak@users.noreply.github.com>
2021-08-28 15:15:13 +0200

    Added new forlders to copy by the .sh script

--- pax/sync_overrides.sh
+++ pax/sync_overrides.sh
@@ -9,6 +9,33 @@ cd ../../..
 cp -r config pax/modpack/overrides
 cd pax
 
+# local
+cd modpack/overrides
+if [ -d "local" ]; then
+    rm -rf local
+fi
+cd ../../..
+cp -r local pax/modpack/overrides
+cd pax
+
+# oresources
+cd modpack/overrides
+if [ -d "oresources" ]; then
+    rm -rf oresources
+fi
+cd ../../..
+cp -r oresources pax/modpack/overrides
+cd pax
+
+# patchouli_books
+cd modpack/overrides
+if [ -d "patchouli_books" ]; then
+    rm -rf patchouli_books
+fi
+cd ../../..
+cp -r patchouli_books pax/modpack/overrides
+cd pax
+
 # resources
 cd modpack/overrides
 if [ -d "resources" ]; then
@@ -19,6 +46,15 @@ cp -r resources pax/modpack/overrides
 cd pax
 
 # scripts
+cd modpack/overrides
+if [ -d "scripts" ]; then
+    rm -rf scripts
+fi
+cd ../../..
+cp -r scripts pax/modpack/overrides
+cd pax
+
+# structures
 cd modpack/overrides
 if [ -d "scripts" ]; then
     rm -rf scripts
