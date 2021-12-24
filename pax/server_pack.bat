57a3979b1732fffca3f1926160d3d389c0da8a6e
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2021-12-24 18:59:58 +0100

    Add ServerPackCreator

---
+++ pax/server_pack.bat
@@ -0,0 +1,6 @@
+
+cd serverpack
+
+cmd /c java -jar ServerPackCreator-3.0.0-alpha.12.jar -cli
+
+echo Done!
---
+++ pax/server_pack.sh
@@ -0,0 +1,6 @@
+
+cd serverpack
+
+java -jar ServerPackCreator-3.0.0-alpha.12.jar -cli
+
+read -p "Done!"
