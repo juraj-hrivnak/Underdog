01defebfc1180fbabdf11da7186317aadc09a458
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2022-05-21 19:38:35 +0200

    Update PAX

--- pax/sync_mods.bat
+++ pax/sync_mods.bat
@@ -2,16 +2,16 @@
 cd ..
 if not exist "mods" mkdir "mods"
 cd mods
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+for /f "delims=" %%i in ('dir /b') do (rmdir "%%i" /s /q || del "%%i" /s /q)
 cd ..\pax\modpack\overrides
-xcopy /s/q mods ..\..\..\mods\
+xcopy /s /q mods ..\..\..\mods\
 cd ..\..\.out
-xcopy /s/q missing_mods ..\..\mods\
+xcopy /s /q missing_mods ..\..\mods\
 cd ..
 
 cmd /c java -jar ModpackDownloader-cli-0.7.2.jar -manifest modpack/manifest.json -folder ../mods
 
 cd ..\mods
-del /s/q *.zip
+del /s /q *.zip
 
 echo Done!
