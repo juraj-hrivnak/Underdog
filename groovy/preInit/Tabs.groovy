33b5018f00ac9831c07982fe7286cc35c27fdc8e
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-10-10 18:10:52 +0200

    Remove tabs

---
+++ groovy/preInit/Tabs.groovy
@@ -0,0 +1,7 @@
+
+import com.cleanroommc.tabulator.common.TabulatorAPI
+
+TabulatorAPI.removeTab('materials.base')
+TabulatorAPI.removeTab('charset')
+TabulatorAPI.removeTab('TinkerWorld')
+TabulatorAPI.removeTab('scalinghealth')
